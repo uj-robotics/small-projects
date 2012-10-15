35a4cff186215986420288e10721bf12eb0e6697
bogoslawek <bogoslawek@3cd3786a-b1df-45d4-8868-78c2fd657d00>
2012-10-15 13:25:15 +0000

    Siedziałem dziś na Wydziale w sali robotyki (bo mi zajecia wypadły) i w końcu obadałem te nowe czujniki kolorów, które kupiliśmy przed wakacjami. Znalazłem kod do starszej wersji lejosa i przerobiłem ją na wersję jakiej używamy. Jeszcze trochę tu posiedzę i zacznę klepać PID'a.

---
+++ LineLeaderExample/src/LineFollower.java
@@ -0,0 +1,51 @@
+import lejos.nxt.*;
+import lejos.nxt.addon.NXTLineLeader;
+
+
+public class LineFollower {
+
+	public static void wait(int milliseconds) throws InterruptedException {
+        Thread.sleep(milliseconds);
+     }
+	
+	public static void main(String[] args) throws InterruptedException {
+		NXTLineLeader liner = new NXTLineLeader(SensorPort.S3);
+		liner.wakeUp();
+		LCD.drawString("Light settings: ", 0, 0);
+        LCD.drawString("Place robot over", 0, 1);
+        LCD.drawString("white area ", 0, 2);
+        LCD.drawString("Press orange btn", 0, 3);
+        Button.ENTER.waitForPressAndRelease();
+        LCD.drawString("**** wait ******", 0, 3);
+        liner.calibrate(NXTLineLeader.LineColor.WHITE);
+        wait(150);
+        LCD.clear();
+        LCD.drawString("Place robot over", 0, 4);
+        LCD.drawString("black area ", 0, 5);
+        LCD.drawString("Press orange btn", 0, 6);
+        Button.ENTER.waitForPressAndRelease();
+        LCD.drawString("**** wait ******", 0, 6);
+        liner.calibrate(NXTLineLeader.LineColor.BLACK);
+        LCD.clear();
+        LCD.drawString("SetPoint=" + liner.getSetPoint(), 0, 0);
+        LCD.drawString("Kp=" + liner.getKP() + " " + "KpDiv=" + liner.getKPDivisor(), 0, 1);
+        LCD.drawString("Ki=" + liner.getKI() + " " + "KiDiv=" + liner.getKIDivisor(), 0, 2);
+        LCD.drawString("Kd=" + liner.getKD() + " " + "KdDiv=" + liner.getKDDivisor(), 0, 3);
+        LCD.drawString("Steering = ", 0, 5);
+        LCD.drawString("Result   = ", 0, 6);
+        LCD.drawString("Average  = ", 0, 7);
+        
+        Button.ENTER.waitForPressAndRelease();
+        
+        while (!Button.ESCAPE.isPressed()) {
+           LCD.drawString("      ", 11, 5);
+           LCD.drawString("      ", 11, 6);
+           LCD.drawString("      ", 11, 7);
+           LCD.drawInt(liner.getSteering(), 11, 5);
+           LCD.drawInt(liner.getResult(), 11, 6);
+           LCD.drawInt(liner.getAverage(), 11, 7);
+           wait(100);
+        }
+	}
+
+}
